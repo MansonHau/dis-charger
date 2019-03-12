899f44d347f69db1e7bf0ab65fdebb7d8b14e283
Manson Hau <34991227+MansonHau@users.noreply.github.com>
2019-03-12 18:24:00 -0400

    Add files via upload
    Added reset functionality, changed stop point

--- _dis_charger.ino
+++ _dis_charger.ino
@@ -2,15 +2,19 @@
 // Pin declarations
 int load_control = 10;
 int voltage_monitor = A0;
+int reset = 7;
 
 int battery_volts = 0;
-int stop_point = 100; // Modify this as needed: Stop point for 12V lead acid between 10 to 12 volts.
+int stop_point = 400; // Modify this as needed: Stop point for 12V lead acid between 10 to 12 volts.
 bool test_complete = false; // Declares the test complete
+int reset_status = 0; // restart the test
 
 void setup() {
   // Pin configurations
   pinMode(load_control, OUTPUT);
   pinMode(voltage_monitor, INPUT);
+  pinMode(reset, INPUT);
+  Serial.begin(115200);
 
   }
 
@@ -18,6 +22,7 @@ void loop() {
   // Check the incoming voltage to begin the discharge test
 
   battery_volts = analogRead(voltage_monitor);
+  reset_status = digitalRead(reset);
 
   // Begin discharging if voltage is high enough
   if (battery_volts > stop_point && test_complete == false) {
@@ -31,6 +36,12 @@ void loop() {
     test_complete = true;
   }
 
+  // Reset the test
+  if (reset_status == HIGH) {
+    test_complete = false;
+  }
+
+  Serial.println(battery_volts);
   delay(500);
   
 
